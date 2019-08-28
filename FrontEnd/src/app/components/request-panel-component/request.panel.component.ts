7730c8bb9cf87e87b3d2ced268721c62862a97ac
Dyomin Victor <vdyo@ciklum.com>
2019-08-28 16:19:42 +0300

    "Detect Indentation" issue in VSCode

--- FrontEnd/src/app/components/request-panel-component/request.panel.component.ts
+++ FrontEnd/src/app/components/request-panel-component/request.panel.component.ts
@@ -2,19 +2,19 @@ import { Component } from '@angular/core';
 import { RequestProvider } from 'src/app/providers/request.provider';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'request.panel.component.html'
+    selector: 'app-root',
+    templateUrl: 'request.panel.component.html'
 })
 export class RequestPanelComponent {
-  requestStatusCode: number = 400;
-  title = 'frontend';
-  responseData = undefined;
-  constructor(
-    private requestProvider: RequestProvider) {
+    requestStatusCode: number = 400;
+    title = 'frontend';
+    responseData = undefined;
+    constructor(
+        private requestProvider: RequestProvider) {
 
-  }
+    }
 
-  requestJwtToken() {
-    this.requestProvider.requestJwtToken().subscribe();
-  }
+    requestJwtToken() {
+        this.requestProvider.requestJwtToken().subscribe();
+    }
 }
