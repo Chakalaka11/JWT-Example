292713ddb18a16587aa9aee53cbd7e72f34a247d
Dyomin Victor <dyomin_vic@ukr.net>
2019-09-05 23:57:33 +0300

    something something

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -3,8 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { ApiConfiguration } from './app/configurations/api.configuration';
 
 if (environment.production) {
+  ApiConfiguration.Host = environment.host;
   enableProdMode();
 }
 
